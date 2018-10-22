16c6393966936ca31b6216501b23b7733a2e1610
thang vu quoc <vuquocthang63@gmail.com>
2018-10-22 18:23:41 +0700

    fix delete clone

---
+++ app/PassCheckpointHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+
+class PassCheckpointHelper
+{
+    /**
+     * @param $listName : mảng tên . ex: [ '0' => 'Vương', '1' => 'Phú' ]
+     * @param $imageLink : link ảnh . ex: http://fb.com/e.jpg
+     */
+    public static function checkImage($listName, $imageLink){
+
+
+        return $index = -1;
+    }
+
+}
